047863b31ed7eb0b19241828fafe06229c844267
Andrey <dev.andr@yandex.com>
2023-02-03 20:49:35 +0500

    created new project

---
+++ src/main/java/ru/kalan/filereaderapp/FileReaderAppApplication.java
@@ -0,0 +1,13 @@
+package ru.kalan.filereaderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileReaderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileReaderAppApplication.class, args);
+    }
+
+}
