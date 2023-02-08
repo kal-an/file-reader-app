bde307d9ed02e6d7cdc74e2bf714f8f3705f97f1
Andrey <dev.andr@yandex.com>
2023-02-08 10:33:51 +0500

    feat: created model SubChapter

---
+++ src/main/java/ru/kalan/filereaderapp/model/SubChapter.java
@@ -0,0 +1,13 @@
+package ru.kalan.filereaderapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubChapter {
+
+    private String title;
+    private Object property;
+
+}
