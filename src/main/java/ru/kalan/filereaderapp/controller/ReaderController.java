b88a4c92154d643df85f8317b1597543d0d34f72
Andrey <dev.andr@yandex.com>
2023-02-08 12:26:48 +0500

    fix: changed return type

--- src/main/java/ru/kalan/filereaderapp/controller/ReaderController.java
+++ src/main/java/ru/kalan/filereaderapp/controller/ReaderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import ru.kalan.filereaderapp.model.Book;
 import ru.kalan.filereaderapp.service.ReaderService;
 
 @RestController
@@ -20,7 +21,7 @@ public class ReaderController {
     private final ReaderService service;
 
     @PostMapping(value = "/read", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Object> readBook(@RequestPart MultipartFile file) {
+    public ResponseEntity<Book> readBook(@RequestPart MultipartFile file) {
         return ResponseEntity.ok(service.createBook(file));
     }
 
