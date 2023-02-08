c48ae658da116b1ed3913de14b10e791f733d30d
Andrey <dev.andr@yandex.com>
2023-02-08 12:43:51 +0500

    feat: added javadoc to ReaderService

--- src/main/java/ru/kalan/filereaderapp/service/ReaderService.java
+++ src/main/java/ru/kalan/filereaderapp/service/ReaderService.java
@@ -3,8 +3,16 @@ package ru.kalan.filereaderapp.service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.kalan.filereaderapp.model.Book;
 
+/**
+ * Сервис чтения файлов получения книги
+ */
 public interface ReaderService {
 
+    /**
+     * Создание книги
+     * @param file {@link MultipartFile}
+     * @return книга {@link Book}
+     */
     Book createBook(MultipartFile file);
 
 }
