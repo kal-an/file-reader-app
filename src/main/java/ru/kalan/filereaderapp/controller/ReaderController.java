2116057a67db4dbb5470b234be9f2e387fe51fd6
Andrey <dev.andr@yandex.com>
2023-02-08 12:43:33 +0500

    feat: added swagger description to controller

--- src/main/java/ru/kalan/filereaderapp/controller/ReaderController.java
+++ src/main/java/ru/kalan/filereaderapp/controller/ReaderController.java
@@ -1,5 +1,8 @@
 package ru.kalan.filereaderapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -16,12 +19,16 @@ import ru.kalan.filereaderapp.service.ReaderService;
 @Slf4j
 @RequestMapping(path = "/api")
 @RequiredArgsConstructor
+@Tag(name = "Читатель", description = "Чтение текстовых файлов")
 public class ReaderController {
 
     private final ReaderService service;
 
     @PostMapping(value = "/read", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Book> readBook(@RequestPart MultipartFile file) {
+    @Operation(summary = "Чтение книги",
+            description = "Позволяет прочитать книгу из файла")
+    public ResponseEntity<Book> readBook(
+            @RequestPart @Parameter(description = "Текстовый файл", required = true) MultipartFile file) {
         return ResponseEntity.ok(service.createBook(file));
     }
 
